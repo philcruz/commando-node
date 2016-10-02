f97e504fb8f5691c3d023d1309a4a18005c61a4a
Phil Cruz <phil@philcruz.com>
2016-10-02 11:05:08 -0700

    implement Executions.js

--- test/commando.js
+++ test/commando.js
@@ -6,11 +6,11 @@ var	assert = require('chai').assert;
 var	commando = require('../lib/commando')(testUtils.getAlias(), testUtils.getSecret());
 
 describe('#config', function() {
-	it('gets the default alias', function() {
+	it('gets the alias', function() {
 		commando.config().alias.should.equal(process.env.COMMANDO_ALIAS || 'your-account-alias');
 	});
 
-	it('gets the default secret', function() {
+	it('gets the secret', function() {
 		commando.config().secret.should.equal(process.env.COMMANDO_SECRET ||'your-secret-key');
 	});
 
@@ -18,7 +18,7 @@ describe('#config', function() {
 
 describe('#servers', function() {
 
-	it('should get the servers', function(done) {
+	it('should get all servers', function(done) {
 		assert.doesNotThrow(function() {
 			commando.servers.list(function(res) {
 				var body = '';
@@ -37,7 +37,7 @@ describe('#servers', function() {
 		});
 	});
 
-	it('should get the server info', function(done) {
+	it('should get a server by id', function(done) {
 		assert.doesNotThrow(function() {
 			commando.servers.retrieve('srv_2vhZl57daf0e630261sHn', function(res) {
 				var body = '';
@@ -60,7 +60,7 @@ describe('#servers', function() {
 
 describe('#groups', function() {
 
-	it('should get the groups', function(done) {
+	it('should get all groups', function(done) {
 		assert.doesNotThrow(function() {
 			commando.groups.list(function(res) {
 				var body = '';
@@ -79,7 +79,7 @@ describe('#groups', function() {
 		});
 	});
 
-	it('should get the group info', function(done) {
+	it('should get a group by id', function(done) {
 		assert.doesNotThrow(function() {
 			commando.groups.retrieve('grp_GDI6t57daf0b5ae5dd168', function(res) {
 				var body = '';
@@ -102,7 +102,7 @@ describe('#groups', function() {
 
 describe('#recipes', function() {
 
-	it('should get the recipes', function(done) {
+	it('should get all recipes', function(done) {
 		assert.doesNotThrow(function() {
 			commando.recipes.list(function(res) {
 				var body = '';
@@ -121,7 +121,7 @@ describe('#recipes', function() {
 		});
 	});
 
-	it('should get the recipe info', function(done) {
+	it('should get a recipe by id', function(done) {
 		assert.doesNotThrow(function() {
 			commando.recipes.retrieve('rec_7hgCk57db09ddc13f0gbx', function(res) {
 				var body = '';
@@ -144,7 +144,7 @@ describe('#recipes', function() {
 
 describe('#execution-queue', function() {
 
-	it('should get the execution-queue', function(done) {
+	it('should get all in the execution-queue', function(done) {
 		assert.doesNotThrow(function() {
 			commando.executionQueue.list(function(res) {
 				var body = '';
@@ -163,7 +163,7 @@ describe('#execution-queue', function() {
 		});
 	});
 
-	it('should get the execution-queue info', function(done) {
+	it('should get an execution-queue by id', function(done) {
 		assert.doesNotThrow(function() {
 			commando.executionQueue.retrieve('57db6511179db941208b456f', function(res) {
 				var body = '';
@@ -183,6 +183,30 @@ describe('#execution-queue', function() {
 	});
 
 });
+
+describe('#executions', function() {
+
+	it('should get an execution by id', function(done) {
+		assert.doesNotThrow(function() {
+			commando.executionQueue.retrieve('57db6511179db941208b456f', function(res) {
+				var body = '';
+				res.on('data', function(chunk){
+					body += chunk;
+				});
+
+				res.on('end', function(){
+					//console.log(body);
+					var data = JSON.parse(body);
+					assert.equal(data.url,'/v1/execution-queue/57db6511179db941208b456f');
+					done();
+				});
+
+			}, done);
+		});
+	});
+
+});
+
 
 describe('#about', function() {
 
